a5cb75e75b3eca7f4fab43bf4e33b13b3ad77562
lolyrealy <razafiarisoamaillole@gmail.com>
2018-02-27 14:59:05 +0300

    avant stage

---
+++ maillole/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="fr">
+<head>
+<meta charset="utf-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://jqueryvalidation.org/files/demo/site-demos.css">
+ 
+</head>
+<body>
+<div class="col-sm-4">
+	<h1 class="text-center">Fidirana</h1>
+<form id="form" action="save.php" method="POST" class="well form-group" >
+		<label for="nom">Anarana : </label>
+		<input type="text" id="nom" name="nom" class="">
+		<p id="content" class="label label-default"></p>
+<br/>
+		<label>Solon anarana:</label><input type="text" class="form-control" >
+		<labe>Adiresy mailaka :</label><input type="email" class="form-control">
+		<label>Teny miafina:</label><input type="Password" class="form-control">
+		<button type="submit" class="btn btn-block btn">Vita tompoko</button>
+</form>
+</div>
+<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
+<script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/additional-methods.min.js"></script>
+<script src="app.js"> </script>
+</body>
+</html>
+
+
